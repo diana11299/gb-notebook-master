39e66ea4b7ee16e7b01b08e6fd116acd2c8518b7
Diana <moyseeko@gmail.com>
2024-01-18 20:02:23 +0300

    dz

---
+++ src/notebook/model/repository/impl/UserRepository.java
@@ -0,0 +1,96 @@
+package notebook.model.repository.impl;
+
+import notebook.model.dao.impl.FileOperation;
+import notebook.util.mapper.impl.UserMapper;
+import notebook.model.User;
+import notebook.model.repository.GBRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserRepository implements GBRepository {
+    private final UserMapper mapper;
+    private final FileOperation operation;
+
+    public UserRepository(FileOperation operation) {
+        this.mapper = new UserMapper();
+        this.operation = operation;
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<String> lines = operation.readAll();
+        List<User> users = new ArrayList<>();
+        for (String line : lines) {
+            users.add(mapper.toOutput(line));
+        }
+        return users;
+    }
+
+    @Override
+    public User create(User user) {
+        List<User> users = findAll();
+        long max = 0L;
+        for (User u : users) {
+            long id = u.getId();
+            if (max < id){
+                max = id;
+            }
+        }
+        long next = max + 1;
+        user.setId(next);
+        users.add(user);
+        write(users);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> update(Long userId, User update) {
+        List<User> users = findAll();
+        User editUser = users.stream()
+                .filter(u -> u.getId()
+                        .equals(userId))
+                .findFirst().orElseThrow(() -> new RuntimeException("User not found"));
+                if(!update.getFirstName().isEmpty()){
+                    editUser.setFirstName(update.getFirstName());    
+                }
+                if(!update.getLastName().isEmpty()){
+                    editUser.setLastName(update.getLastName());
+                }
+                if(!update.getPhone().isEmpty()){
+                    editUser.setPhone(update.getPhone());
+                }
+        
+        write(users);
+        return Optional.of(update);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        List<User> users = findAll();
+        User user = users.stream()
+                .filter(u -> u.getId()
+                        .equals(id))
+                .findFirst().orElseThrow(() -> new RuntimeException("User not found"));
+
+        users.remove(user);
+
+        write(users);
+        return true;
+    }
+
+    private void write(List<User> users) {
+        List<String> lines = new ArrayList<>();
+        for (User u: users) {
+            lines.add(mapper.toInput(u));
+        }
+        operation.saveAll(lines);
+    }
+
+}
